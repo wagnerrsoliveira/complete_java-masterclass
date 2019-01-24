cdeaa2f8f706f7aa1467180eb4d0499cdbbab78f
Wagner Roberto <wagnerrsolivera@gmail.com>
2019-01-24 13:11:30 -0200

    More On Operators And Operator Precedence

--- src/br/com/wagnerroberto/variables_datatypes_and_operators/Operators.java
+++ src/br/com/wagnerroberto/variables_datatypes_and_operators/Operators.java
@@ -53,7 +53,42 @@ public class Operators {
             System.out.println("You got the high score!");
 
         int secondScore = 60;
-        if (topScore > secondScore && topScore < 100)
+        if ((topScore > secondScore) && (topScore < 100))
             System.out.println("Greater than second score and less than 100");
+
+        if ((topScore > 90) || (secondScore <= 90))
+            System.out.println("One of these tests is true");
+
+        int newValue = 50;
+        if (newValue == 50)
+            System.out.println("This is true");
+
+        boolean isCar = false;
+        if (isCar)
+            System.out.println("This is not supposed to happen");
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar)
+            System.out.println("wasCar is true");
+
+        // 1. Create a double variable with the value 20
+        // 2. Create a second variable of type double with the value 80
+        // 3. Add both numbers up an multiply by 25
+        // 4. Use the remainder operator to figure out the remainder from the sum of #3 divided by 40
+        // 5. Write an "if" statement that displays a massage "Total was over the limit"
+        //    if the remaining total (#4) is equal to 20 or less.
+        double myFirstValue = 20d;
+        double mySecondValue = 80d;
+        double myTotal = (myFirstValue + mySecondValue) * 25;
+        System.out.println("myTotal = "+ myTotal);
+        double theRemainder = myTotal % 40;
+        System.out.println("Remainder is " + theRemainder);
+        if (theRemainder <= 20)
+            System.out.println("Total was over the limit");
+
+
+
+
     }
 }
