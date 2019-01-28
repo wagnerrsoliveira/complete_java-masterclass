b75db5002d032e1568e45f6963310468732d8245
Wagner Roberto <wagnerrsolivera@gmail.com>
2019-01-28 12:48:14 -0200

    Keywords And Expressions

---
+++ src/br/com/wagnerroberto/java_tutorial_expressions_statements_code_blocks_methods_and_more/KeywordsAndExpressions.java
@@ -0,0 +1,22 @@
+package br.com.wagnerroberto.java_tutorial_expressions_statements_code_blocks_methods_and_more;
+
+public class KeywordsAndExpressions {
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometres
+        double  kilometres = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50){
+            System.out.println("This is a expression");
+        }
+
+        // In the following code that I will type below, write down what parts of the code
+        // are expressions.
+        int score = 100;
+        if (score > 99){
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+
+    }
+}
