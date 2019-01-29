972adbbd7b36753979bbb143e98a7d12c9b829ff
Wagner Roberto <wagnerrsolivera@gmail.com>
2019-01-29 14:24:11 -0200

    Code Blocks And The If Then Else Control Statements

---
+++ src/br/com/wagnerroberto/java_tutorial_expressions_statements_code_blocks_methods_and_more/IfKeywordAndCodeBlocks.java
@@ -0,0 +1,50 @@
+package br.com.wagnerroberto.java_tutorial_expressions_statements_code_blocks_methods_and_more;
+
+public class IfKeywordAndCodeBlocks {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your score was less than 5000 but greater than 1000");
+        } else if (score < 1000) {
+            System.out.println("Your score was less than 1000");
+        } else {
+            System.out.println("Got here");
+        }
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        // Print out a second score on the screen with the following
+        // score set 10000
+        // levelCompleted set to 8
+        // bonus set 200
+        // but make sure the first print out above still displays as well
+
+        boolean newGameOver = true;
+        int newScore = 10000;
+        int newLevelCompleted = 8;
+        int newBonus = 200;
+
+        if (newGameOver) {
+            int finalScore = newScore + (newLevelCompleted * newBonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+}
